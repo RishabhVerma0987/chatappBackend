ddf7d49664f14b522f67ba4c2dcd039f1e95fbad
rishabh verma <vermarishabh0987@gmail.com>
2020-07-09 13:33:23 +0530

    basic v1 done

--- server.js
+++ server.js
@@ -1,14 +1,30 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const app = express();
-
+const socket = require("socket.io");
+const color = require("colors");
+const cors = require("cors");
 dotenv.config({ path: "../backend/config/config.env" });
 
 const port = process.env.PORT || 5000;
 
-app.listen(
+app.use(cors());
+var server = app.listen(
   port,
   console.log(
-    `Server is running in ${process.env.NODE_ENV} on port`.yellow.bold
+    `Server is running in ${process.env.NODE_ENV} on port ${process.env.PORT} `
+      .yellow.bold
   )
 );
+
+// Socket setup & pass server
+var io = socket(server);
+io.on("connection", (socket) => {
+  console.log("made socket connection".green.bold, socket.id);
+
+  // Handle chat event
+  socket.on("chat", function (data) {
+    console.log(data);
+    io.sockets.emit("chat", data);
+  });
+});
