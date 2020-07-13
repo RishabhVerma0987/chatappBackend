c6b176daffcc74365fd9893572a5ebb76c5188df
rishabh verma <vermarishabh0987@gmail.com>
2020-07-13 23:56:53 +0530

    some backend

---
+++ dummyuser.js
@@ -0,0 +1,30 @@
+const users = [];
+
+// Join user to chat
+function userJoin(id, username, room) {
+  const user = { id, username, room };
+
+  users.push(user);
+
+  return user;
+}
+
+// Get current user
+function getCurrentUser(id) {
+  return users.find((user) => user.id === id);
+}
+
+// User leaves chat
+function userLeave(id) {
+  const index = users.findIndex((user) => user.id === id);
+
+  if (index !== -1) {
+    return users.splice(index, 1)[0];
+  }
+}
+
+module.exports = {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+};
--- server.js
+++ server.js
@@ -6,10 +6,12 @@ const morgan = require("morgan");
 const color = require("colors");
 const cors = require("cors");
 const connetDB = require("./config/db.js");
+const { getCurrentUser, userLeave, userJoin } = require("./dummyuser");
 const errorHandler = require("./middleware/errorhandler.js");
 dotenv.config({ path: "../backend/config/config.env" });
 
 const auth = require("./routes/auth.js");
+const { use } = require("./routes/auth.js");
 
 app.use(morgan("dev"));
 app.use(express.json());
@@ -30,4 +32,57 @@ var server = app.listen(
 process.on("unhandledRejection", (err, promisee) => {
   console.log(`Error : ${err.message}`.red.bold);
   server.close(() => process.exit(1));
+});
+
+// initialized
+const io = socket(server);
+
+//everything related to io will go here
+io.on("connection", (socket) => {
+  //when new user join room
+  socket.on("joinRoom", ({ username, roomname }) => {
+    //* create user
+    const user = userJoin(socket.id, username, roomname);
+    socket.join(user.room);
+
+    //* emit message to user to welcome him/her
+    socket.emit("message", {
+      userId: user.id,
+      username: user.username,
+      text: `Welcome ${user.username}`,
+    });
+
+    //* Broadcast message to everyone except user that he has joined
+    socket.broadcast.to(user.room).emit("message", {
+      userId: user.id,
+      username: user.username,
+      text: `${user.username} has joined the chat`,
+    });
+  });
+
+  //when somebody send text
+  socket.on("chat", (text) => {
+    //* get user room and emit message
+    const user = getCurrentUser(socket.id);
+
+    io.to(user.room).emit("message", {
+      userId: user.id,
+      username: user.username,
+      text: text,
+    });
+  });
+
+  // Disconnect , when user leave room
+  socket.on("disconnect", () => {
+    // * delete user from users & emit that user has left the chat
+    const user = userLeave(socket.id);
+
+    if (user) {
+      io.to(user.room).emit("message", {
+        userId: user.id,
+        username: user.username,
+        text: `${user.usernam} has left the chat`,
+      });
+    }
+  });
 });
